7dbf9fdaecd29c27f5e684b0f6002dd34696e8e7
TargiX <targix8@gmail.com>
2019-01-20 23:32:40 +0600

    Updates

---
+++ precache-manifest.1b7b900c95360fca23ef673e0c9f1b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65dd574dc71532402377",
+    "url": "/Contact-Manager/static/js/runtime~main.65dd574d.js"
+  },
+  {
+    "revision": "998fe54508c25e2a0a36",
+    "url": "/Contact-Manager/static/js/main.998fe545.chunk.js"
+  },
+  {
+    "revision": "dcc6f2473f3d85fcbd1b",
+    "url": "/Contact-Manager/static/js/1.dcc6f247.chunk.js"
+  },
+  {
+    "revision": "998fe54508c25e2a0a36",
+    "url": "/Contact-Manager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "dcc6f2473f3d85fcbd1b",
+    "url": "/Contact-Manager/static/css/1.d70ea7d1.chunk.css"
+  },
+  {
+    "revision": "c384d78451c66622abb33cd935724223",
+    "url": "/Contact-Manager/index.html"
+  }
+];
